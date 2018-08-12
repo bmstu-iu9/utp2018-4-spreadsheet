05c35d849a6d51d82048c689e6a4c768d6bea518
AntonPriyma <apriyma0@gmail.com>
2018-08-12 13:44:48 +0300

    index.js edited

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const logs = require('../app/logs');
 
 const index = (req, res) => {
     logs.log('\x1b[34mINDEX\x1b[0m Method: ' + req.method);
-    res.render('edit-page.html', null);
+    res.render('index.html', null);
 }
 
 module.exports.index = index;
