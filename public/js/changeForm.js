45a94eb11e78ab620c0721623345a34ac83dde5f
Baklanov Lev <penachett@bk.ru>
2018-08-17 08:38:52 +0300

    fixed animation bug

--- public/js/changeForm.js
+++ public/js/changeForm.js
@@ -1,22 +1,20 @@
 function toLogin() {
     regForm = document.getElementById('reg_form');
     regForm.style.display = 'none';
-    document.getElementById('log_form').style.display = 'block';
 //    document.body.style.top = '275px';
     document.title = 'Register in Data';
-    animate_login();
+    animateLogin();
 }
     
 function toRegister() {
     logForm = document.getElementById('log_form');    
     logForm.style.display = 'none';
-    document.getElementById('reg_form').style.display = 'block';
     document.title = 'LogIn in Data';
 //    document.body.style.top = '200px';
-    animate_register();
+    animateRegister();
 }
             
-function animate_login() {
+function animateLogin() {
     let start = Date.now();
     let timer = setInterval(function() {
         let timePassed = Date.now() - start;
@@ -24,18 +22,24 @@ function animate_login() {
             clearInterval(timer);
             return;
         }
+        if (timePassed > 0) {
+            document.getElementById('log_form').style.display = 'block';
+        }
         document.body.style.top = 105 + timePassed/4.5 + 'px';
         document.body.style.opacity = timePassed/750;   
     }, 5)
 }
             
-function animate_register() {
+function animateRegister() {
     let start = Date.now();
     let timer = setInterval(function() {
         let timePassed = Date.now() - start;
         if (timePassed >= 750) {
             clearInterval(timer);
             return;
+        }
+        if (timePassed > 0) {
+            document.getElementById('reg_form').style.display = 'block';
         }
         document.body.style.top = timePassed / 4.5 + 'px';  
         document.body.style.opacity = timePassed/750; 
