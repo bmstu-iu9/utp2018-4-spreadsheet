91ce7432367c44acbc889cb2f27429ec55a77813
GDVFox <gaaveer@gmail.com>
2018-08-22 09:42:43 +0300

    fix

--- app/render.js
+++ app/render.js
@@ -3,15 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 
-<<<<<<< HEAD
-=======
 /**
  * Reads html file from views/templateName,
  * replaces {{key}} templates to values from data object 
  * @param {String} templateName 
  * @param {Object} data key-name in html template, value-data in result response
  */
->>>>>>> auth
 function render(templateName, data) {
     fs.readFile(path.resolve('views', templateName), 'utf-8',
         (error, template) => {
@@ -22,25 +19,15 @@ function render(templateName, data) {
                 return this.end('internal server error');
             }
 
-<<<<<<< HEAD
-            var html = template;
-=======
             let html = template;
->>>>>>> auth
             if (data) {
                 html = template.replace(/{{([^{}]*)}}/g, (placeholder, property) => {
                     return data[property] || placeholder;
                 });
             }
 
-<<<<<<< HEAD
-            this.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-=======
             this.statusCode = 200;
             this.setHeader('Content-Type', 'text/html');
->>>>>>> auth
             this.end(html);
         });
 }
