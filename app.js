9928ff92f153739223d129519703f7fd348260ba
GDVFox <gaaveer@gmail.com>
2018-08-22 09:25:56 +0300

    for merge

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ http.ServerResponse.prototype.render = render;
 http.createServer((req, res) => {
     if (req.url === '/') {
         index(req, res);
-    } else if (req.url.match(/\.(html|css|js|png|jpg|svg)$/)){
-        public(req, res);  
+    } else if (req.url.match(/\.(html|css|js|png|jpg|svg)$/)) {
+        public(req, res);
     } else {
-        res.render('error.html', {"code" : 404, "message" : '404 Not Found!'});
+        res.render('error.html', {
+            "code": 404,
+            "message": '404 Not Found!'
+        });
     }
-}).listen(CONFIG.port,() => 
-     console.log('\x1b[35m%s\x1b[0m successfully \x1b[32mstarted\x1b[0m at %d','Main Application',
-                CONFIG.port));
+}).listen(CONFIG.port, () =>
+    console.log('\x1b[35m%s\x1b[0m successfully \x1b[32mstarted\x1b[0m at %d', 'Main Application',
+        CONFIG.port));
